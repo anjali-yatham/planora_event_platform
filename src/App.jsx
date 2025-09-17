e25be86273c02613c8e5d6c5af0051258ec7e3fd
anjali-yatham <divyanjaliyatham@gmail.com>
2025-09-18 01:14:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,3 +8,15 @@ function App() {
 }
 
 export default App;
+import BackendHealthCheck from "./pages/BackendHealthCheck";
+
+function App() {
+  return (
+    <div>
+      <h1>Planora Event Platform</h1>
+      <BackendHealthCheck />
+    </div>
+  );
+}
+
+export default App;

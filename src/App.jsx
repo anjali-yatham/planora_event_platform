6dd2ed41da9d25de2682d07a9cbb1619cb441476
anjali-yatham <divyanjaliyatham@gmail.com>
2025-09-18 01:21:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,3 @@
-import React from "react";
-import Routes from "./Routes";
-
-function App() {
-  return (
-    <Routes />
-  );
-}
-
-export default App;
 import BackendHealthCheck from "./pages/BackendHealthCheck";
 
 function App() {
@@ -20,3 +10,4 @@ function App() {
 }
 
 export default App;
+
